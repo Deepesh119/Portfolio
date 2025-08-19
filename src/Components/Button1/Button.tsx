9da4155adfdb5a81845fef1b5edce4a3554dd1d2
Deepesh Giri <89577491+Deepesh119@users.noreply.github.com>
2025-08-19 21:17:52 +0530

    Update Button.tsx
    Button Chnaged Ts file

--- src/Components/Button1/Button.tsx
+++ src/Components/Button1/Button.tsx
@@ -1,4 +1,3 @@
-import Button from '@mui/material/Button';
 import styles from "./index.module.css";
 
 type ButtonViewProps = {
@@ -8,27 +7,12 @@ type ButtonViewProps = {
 
 const  ButtonView=({text,onClick}:ButtonViewProps)=> {
   return (
-       <Button
-        sx={{
-          border: "1px solid white",
-          width: "200px",
-          height: "60px",
-          backgroundColor: "#000",
-          color: "white",
-          "&:hover": {
-            backgroundColor: "#000",
-            color: "white",         
-          },
-          "&:active": {
-            backgroundColor: "#000", 
-            color: "white",
-          },
-        }}
-        variant="outlined"
+       <button
+        className={styles.btnstyle}
         onClick={onClick}
     >
-      <span style={{color:"white"}} className={styles.textColor}>{text}</span>
-    </Button>
+      {text}
+    </button>
   );
 }
 
