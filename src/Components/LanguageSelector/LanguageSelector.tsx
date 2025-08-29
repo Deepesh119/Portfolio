e6215bef236e5eb8d54b8b6936ed095cf0ef8b0f
Deepesh Giri <89577491+Deepesh119@users.noreply.github.com>
2025-08-29 19:44:31 +0530

    6

--- src/Components/LanguageSelector/LanguageSelector.tsx
+++ src/Components/LanguageSelector/LanguageSelector.tsx
@@ -1,37 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import styles from "./Index.module.css"
-import Globe from "../../assets/Globe.jpg"
+import styles from "./index.module.css"
 
 const LanguageSelector = () => {
   const { i18n } = useTranslation();
   const [language, setLanguage] = useState(i18n.language || "en");
+  const [theme, setTheme] = useState('dark')
 
-  const handleChange = (e:any) => {
+  const handleChange = (e: any) => {
     const selectedLanguage = e.target.value;
     setLanguage(selectedLanguage);
-    i18n.changeLanguage(selectedLanguage); 
+    i18n.changeLanguage(selectedLanguage);
+  };
+ 
+
+  const handleChangeTheme = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+    document.body.className = theme;
+    localStorage.setItem("theme", theme);
   };
 
+
   return (
-   <div className={styles.parent}> 
-    <div className={styles.container}>
-    <select
-      role="select"
-      value={language}
-      onChange={handleChange}
-      className={styles.select}
-      name="DropDown Language Selector"
-    >
-      <option value="en" >ENGLISH</option>
-      <option value="fr">FRENCH</option>
-      <option value="de">GERMAN</option>
-    </select>
-   </div> 
-    <img src={Globe} alt="Language" className={styles.languageIcon}/>
-  </div> 
+    <div className={styles.parent}>
+      <div className={styles.container}>
+        <select
+          role="select"
+          value={language}
+          onChange={handleChange}
+          className={styles.select}
+          name="DropDown Language Selector"
+        >
+          <option value="en" >ENGLISH</option>
+          <option value="fr">FRENCH</option>
+          <option value="de">GERMAN</option>
+        </select>
+      </div>
+      <div style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
+      <span style={{fontSize:"40px"}}onClick={handleChangeTheme}>{theme === "light" ? <>🌑</> :<>☀️</> }</span>
+      </div>
+    </div>
   );
 };
 
 export default LanguageSelector;
-
